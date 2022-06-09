923f374a98fa5cd76199e2fce4f981e8433760b5
tyange <usun16@gmail.com>
2022-06-09 14:50:58 +0900

    styles 폴더 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-import "./App.css";
+import "./styles/App.css";
 import Header from "./components/Header";
 import Movie from "./components/Movie";
 import Search from "./components/Search";
