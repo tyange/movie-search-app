56053aa1aa55324a15cb58894b62a85613e057f6
tyange <usun16@gmail.com>
2022-06-09 12:36:06 +0900

    search 마크업

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Movie from "./components/Movie";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <Header text="HOOKED" />
+      <Search />
       <main>
         <Movie />
       </main>
---
+++ src/components/Search.tsx
@@ -0,0 +1,10 @@
+const Search = () => {
+  return (
+    <form className="search">
+      <input type="text" />
+      <input type="submit" value="Search" />
+    </form>
+  );
+};
+
+export default Search;
