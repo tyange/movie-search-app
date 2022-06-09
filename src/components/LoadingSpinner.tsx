530ab7d48b5bb92990f96ed133d2f55c6c5f13ce
tyange <usun16@gmail.com>
2022-06-09 14:51:08 +0900

    로딩 스피너 추가

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,14 @@
+import "../styles/LoadingSpinner.css";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
