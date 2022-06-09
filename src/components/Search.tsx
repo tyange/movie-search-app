2801341f19c501feef6946b1d5c5a1031797fbf6
tyange <usun16@gmail.com>
2022-06-09 13:21:43 +0900

    add search logic

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,32 @@
+import { useState, FormEvent } from "react";
+
 const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchInputChanges = (text: string) => {
+    setSearchValue(text);
+  };
+
+  const resetInputField = () => {
+    setSearchValue("");
+  };
+
+  const callSearchFunction = (e: FormEvent) => {
+    e.preventDefault();
+
+    // call search function with searchValue
+    console.log(searchValue);
+
+    resetInputField();
+  };
+
   return (
-    <form className="search">
-      <input type="text" />
+    <form className="search" onSubmit={callSearchFunction}>
+      <input
+        type="text"
+        value={searchValue}
+        onChange={(e) => handleSearchInputChanges(e.target.value)}
+      />
       <input type="submit" value="Search" />
     </form>
   );
