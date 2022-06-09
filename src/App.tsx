da549e2af49d3d627c29788977f8d0e8115f586a
tyange <usun16@gmail.com>
2022-06-09 12:32:59 +0900

    movie 마크업

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Movie from "./components/Movie";
 
 function App() {
   return (
     <div className="App">
       <Header text="HOOKED" />
       <main>
-        <p>movie search app</p>
+        <Movie />
       </main>
     </div>
   );
---
+++ src/components/Movie.tsx
@@ -0,0 +1,16 @@
+const DEFAULT_PLACEHOLDER_IMAGE =
+  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+
+const Movie = () => {
+  return (
+    <div className="movie">
+      <h2>movie title</h2>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <p>movie year</p>
+    </div>
+  );
+};
+
+export default Movie;
