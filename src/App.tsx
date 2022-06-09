3b3eb898a35e1e9264d7cfc97fcf95ca06b79d05
tyange <usun16@gmail.com>
2022-06-09 14:46:18 +0900

    data fetching test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Movie from "./components/Movie";
 import Search from "./components/Search";
 
+const MOVIE_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=be88a80b";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const getMovies = useCallback(async () => {
+    try {
+      const response = await fetch(MOVIE_API_URL);
+
+      const responseData = await response.json();
+
+      setMovies(responseData);
+    } catch (err) {
+      setErrorMessage(errorMessage);
+    }
+  }, [errorMessage]);
+
+  useEffect(() => {
+    getMovies();
+  }, [getMovies]);
+
   return (
     <div className="App">
       <Header text="HOOKED" />
       <Search />
-      <main>
-        <Movie />
-      </main>
+      <main></main>
     </div>
   );
 }
